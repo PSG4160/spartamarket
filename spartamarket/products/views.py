2df91225e27e95e8e7363c7ba2ebfcd028235287
PSG4160 <psg4160@gmail.com>
2024-12-16 17:26:00 +0900

    add/products_app/products product_detail

--- spartamarket/products/views.py
+++ spartamarket/products/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
+from .forms import ProductForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -6,4 +9,18 @@ def index(request):
 
 
 def products(request):
+    products = Product.objects.all()
+    context = {
+        "products" : products,
+    }
+    return render(request, 'products/products.html', context)
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        "products" : product,
+    }
+    return render(request, 'products/product_detail.html', context)
+
+def create(request, pk):
     pass
